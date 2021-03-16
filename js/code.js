583eeb5e77121d4dcb9837c51c12380c6af142a9
Arya <ParsWebDesigners@gmail.com>
2021-03-16 04:12:31 +0100

    design code editor

--- js/code.js
+++ js/code.js
@@ -1,17 +1,3 @@
-function RUN()
-{
-  var code = document.getElementById('Code').value;
-    
-  var iframe = document.getElementById('Browser');
-    
-  iframe = (iframe.contentWindow) ? iframe.contentWindow : (iframe.contentDocument.document) ? iframe.contentDocument.document :iframe.contentDocument;
-  
-  iframe.document.open();
-  iframe.document.write(code);
-  iframe.document.close();
-  
-  return false;
-}
 function hide()
 {
     document.getElementById('Code-hide').style.display = 'none';
@@ -32,22 +18,6 @@ function show()
     document.getElementById('Code-hide').style.display = 'block';
     document.getElementById('Output-hide').style.display = 'none';
 }
-
-function size()
-{
-    var width = window.innerWidth;
-    
-    if(width>=550)
-       {document.getElementById('Code-hide').style.display = 'table-cell';
-       document.getElementById('Output-hide').style.display = 'table-cell';} 
-    else
-      {document.getElementById('Code-hide').style.display = 'block';
-      document.getElementById('Output-hide').style.display = 'none';}  
-        
-        
-}
-
-
 
 
 
